ad1d0e2646528e5cab75c76f653289ff2e984ef0
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-08-03 10:34:10 -0700

    Added general tree

---
+++ tree.py
@@ -0,0 +1,53 @@
+from typing import Any
+
+class TreeNode:
+    def __init__(self,data:Any):
+        self.data = data
+        self.children = [] #each of these elements is an instance of TreeNode class
+        self.parent = None
+
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level+=1
+            p=p.parent
+        return level
+
+    def add_child(self, child:'TreeNode'):
+        child.parent = self
+        self.children.append(child)
+
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|_ _" if self.parent else "" 
+        print(prefix+self.data)
+        if len(self.children):
+            for child in self.children:
+                child.print_tree()
+
+def build_product_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("Laptop")
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
+
+    cellphone = TreeNode("Cell Phone")
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Google Pixel"))
+    cellphone.add_child(TreeNode("Vivo"))
+
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("Samsung"))
+    tv.add_child(TreeNode("LG"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+    return root
+
+if __name__ == '__main__':
+    root = build_product_tree()
+    root.print_tree()
