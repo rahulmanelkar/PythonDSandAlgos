beffdbd4244cd1bfbab5c36c35c3d00b4718afcc
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-09-01 21:14:19 -0700

    added main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     print("Hello from pythondsandalgos!")
     print("Which algorithm would you like to see?")
-    print("Choose hxcafffgghhhib hbfafafdddfvfsfh")
+    print("Choose hxcafffgghhhib hbfafafdddffffvfsfh")
 
 
 if __name__ == "__main__":
